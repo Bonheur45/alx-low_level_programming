6cf73b47d61f4e3675bc2e2ef7a511ad53d222b7
Bonheur Mwizerwa <mwizerwabonheur45@gmail.com>
2023-04-18 10:26:13 -0700

    poppy

--- dog.h
+++ dog.h
@@ -2,18 +2,18 @@
 #define DOG_H
 
 /**
- * struct dog - a dog's basic info
+ * struct - a dog's basic info
  * @name: First member
- * @ge: Second member
+ * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
@@ -29,3 +29,4 @@ char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
+
